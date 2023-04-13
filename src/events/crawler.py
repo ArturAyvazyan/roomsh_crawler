97f191a6ed1c6bbf6b09750b5eae95afcd17c591
Артур Айвазян <archiforeverything@gmail.com>
2023-04-13 04:15:41 +0300

    make it infinite

--- src/events/crawler.py
+++ src/events/crawler.py
@@ -14,15 +14,17 @@ keywords = ["магазин", "продукция", "каталог"]
 class Crawler:
     @staticmethod
     async def start():
-        tasks = list()
-        for website in WEBSITES_TO_CRAWL.values():
-            session = requests.session()
-            session.get(website)
+        while True:
+            tasks = list()
+            for website in WEBSITES_TO_CRAWL.values():
+                session = requests.session()
+                session.get(website)
 
-            task = asyncio.create_task(Crawler.crawl(website=website))
-            tasks.append(task)
+                task = asyncio.create_task(Crawler.crawl(website=website))
+                tasks.append(task)
 
-        await asyncio.gather(*tasks)
+            await asyncio.gather(*tasks)
+            await asyncio.sleep(225)  # TODO: think to make it run once/twice a day
 
     @staticmethod
     async def crawl(website: str = None):  # TODO this
@@ -37,5 +39,4 @@ class Scraper:
         print(f"{page = }")
         await asyncio.sleep(25)
         print("outside")
-        # TODO: Get all pages of site
         # TODO: run task to check if there is prices
