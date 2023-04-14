09e32ef89a7dcee0ab4c7418b154bdc70d5a3d03
Артур Айвазян <archiforeverything@gmail.com>
2023-04-14 15:34:38 +0300

    base client+1

--- src/clients/redis_client.py
+++ src/clients/redis_client.py
@@ -9,7 +9,7 @@ class RedisClient(BaseClient):
     def __init__(self):
         self.host = redis_settings["HOST"]
         self.port = redis_settings["PORT"]
-        self.redis_server = redis.Redis(
+        self.server = redis.Redis(
             host=redis_settings["HOST"],
             port=redis_settings["PORT"],
             decode_responses=True,
@@ -22,18 +22,18 @@ class RedisClient(BaseClient):
         ...
 
     def get_shop(self, shop: str) -> ShopModel:
-        shop = self.redis_server.get(shop)
+        shop = self.server.get(shop)
         return ShopModel.parse_obj(shop)
 
     def get_all_shops(self) -> dict[str: ShopModel]:
-        all_shops = self.redis_server.scan()
+        all_shops = self.server.scan()
         result = dict()
         for shop in all_shops:
             result.update({shop: ShopModel.parse_obj(all_shops[shop])})
         return result
 
     def clear_all(self):
-        self.redis_server.flushall()
+        self.server.flushall()
 
     def get_diff(self, shop: ShopModel):
         ...  # FIXME: скорее всего нужно бы реализовать в основном проекте
